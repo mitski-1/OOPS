6f5350f306cc9e399b671a9ba58c1267f1df3f7d
Tanisha <tanishachoudhary1211@gmail.com>
2025-04-16 14:34:48 +0530

    Update FileCreate.java

--- FileCreate.java
+++ FileCreate.java
@@ -1,7 +1,7 @@
 import java.io.*;
 public class FileCreate {
 public static void main(String[] args{
-File file= new File("C:\\Users\\HP\\Desktop\\aditya_04\\Example.txt");
+File file= new File("C:\\Users\\HP\\Desktop\\tanisha_o4\\Example.txt");
 try {
 if(file.createNewFile()) {
 System.out.println("File is Created");
